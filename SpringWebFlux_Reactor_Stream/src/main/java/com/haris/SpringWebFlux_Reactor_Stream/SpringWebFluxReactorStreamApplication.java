641720dfb758358089dfe0e0a5e69cb5d89f9663
harismuzafffer <harismuzaffer@gmail.com>
2018-04-30 15:42:02 +0530

    ;init

---
+++ SpringWebFlux_Reactor_Stream/src/main/java/com/haris/SpringWebFlux_Reactor_Stream/SpringWebFluxReactorStreamApplication.java
@@ -0,0 +1,12 @@
+package com.haris.SpringWebFlux_Reactor_Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxReactorStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebFluxReactorStreamApplication.class, args);
+	}
+}
---
+++ SpringWebFlux_Reactor_Stream/src/test/java/com/haris/SpringWebFlux_Reactor_Stream/SpringWebFluxReactorStreamApplicationTests.java
@@ -0,0 +1,16 @@
+package com.haris.SpringWebFlux_Reactor_Stream;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebFluxReactorStreamApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
